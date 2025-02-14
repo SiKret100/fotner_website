2fd72262a6647ecd268a5fefd4d08453b17e93fc
Dawid Rutkowski <dawidrut01@gmail.com>
2025-02-14 16:20:19 +0100

    add custom fonts to tailwind

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <nav className={"fixed w-full h-24 bg-white shadow-xl"}>
+        <nav className={"fixed w-full h-18 bg-white shadow-xl"}>
             <div className={"flex justify-between items-center h-full px-4 2xl:px-16"}>
                 <Link href="/">
                     <Image
                         src={Logo}
                         alt="Fotner Studio"
-                        width="95"
+                        width="75"
                         className="cursor-pointer"
                     />
                 </Link>
@@ -18,19 +18,19 @@ export default function NavBar() {
 
                     <ul className={"hidden sm:flex"}>
                         <Link href="/">
-                            <li className={"mr-10 uppercase"}>Home</li>
+                            <li className={"font-integral-medium font-bold mr-10"}>Home</li>
                         </Link>
 
                         <Link href="/">
-                            <li className={"mr-10 uppercase"}>O nas</li>
+                            <li className={"font-integral-medium font-bold mr-10"}>O nas</li>
                         </Link>
 
                         <Link href="/">
-                            <li className={"mr-10 uppercase"}>Nasza praca</li>
+                            <li className={"font-integral-medium font-bold mr-10"}>Nasza praca</li>
                         </Link>
 
                         <Link href="/">
-                            <li className={"uppercase"}>Kontakt</li>
+                            <li className={"font-integral-medium font-bold"}>Kontakt</li>
                         </Link>
                     </ul>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,20 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        'integral': ["'Integral CF'", "sans-serif"],
+        'integral-bold': ["'Integral CF'", "sans-serif"], // Handles bold style via CSS
+        'integral-medium': ["'Integral CF'", "sans-serif"], // Handles medium style via CSS
+        'integral-heavy': ["'Integral CF Heavy'", "sans-serif"],
+        'integral-extra': ["'Integral CF Extra'", "sans-serif"],
+        'integral-oblique': ["'Integral CF Oblique'", "sans-serif"],
+        'integral-medium-oblique': ["'Integral CF Medium Oblique'", "sans-serif"],
+        'integral-heavy-oblique': ["'Integral CF Heavy Oblique'", "sans-serif"],
+        'integral-extra-oblique': ["'Integral CF Extra Bold Oblique'", "sans-serif"],
+        'integral-bold-oblique': ["'Integral CF Bold Oblique'", "sans-serif"],
+        'integral-demi-bold': ["'Integral CF Demi'", "sans-serif"],
+        'integral-demi-oblique': ["'Integral CF Demi Bold Oblique'", "sans-serif"],
+      },
     },
   },
   plugins: [],
