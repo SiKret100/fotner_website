bd7a0b90fa17ff89e170191bf56b9793592eaaff
Dawid Rutkowski <dawidrut01@gmail.com>
2025-02-15 16:10:41 +0100

    add animation to portfolio page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,27 @@
-import type { Metadata } from "next";
+'use client';
+
 import "./globals.css";
 import NavBar from "@/components/NavBar";
-
-export const metadata: Metadata = {
-  title: "Fotner Studio",
-  description: "Fotner Studio portfolio",
-};
+import { AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: {
   children: React.ReactNode;
-}>) {
+}) {
+  const pathname = usePathname();
+
   return (
-    <html lang="pl">
-      <body className={"bg-neutral-100"}>
-
-        <NavBar/>
-
-        <main className={"pt-32"} >{children}</main>
-
+      <html lang="pl">
+      <body className="bg-neutral-100">
+      <NavBar />
+      <AnimatePresence mode="wait" initial={false}>
+        <main key={pathname} className="pt-32">
+          {children}
+        </main>
+      </AnimatePresence>
       </body>
-    </html>
+      </html>
   );
 }
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,131 +1,71 @@
-'use client'
+'use client';
 import Image from "next/image";
-import Cover1 from "@/public/portfolio_cover/img_1.jpg"
-import Cover2 from "@/public/portfolio_cover/img_2.jpg"
-import Cover3 from "@/public/portfolio_cover/img_3.jpg"
-import Cover4 from "@/public/portfolio_cover/img_4.jpg"
-import Cover5 from "@/public/portfolio_cover/img_5.jpg"
-import { RiCameraLensLine } from "react-icons/ri";
+import Cover1 from "@/public/portfolio_cover/img_1.jpg";
+import Cover2 from "@/public/portfolio_cover/img_2.jpg";
+import Cover3 from "@/public/portfolio_cover/img_3.jpg";
+import Cover4 from "@/public/portfolio_cover/img_4.jpg";
+import Cover5 from "@/public/portfolio_cover/img_5.jpg";
+import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
+const transition1 = {
+    duration: 1.4,
+    ease: [0.6, 0.01, 0, 0.9],
+};
 
 export default function PortfolioPage() {
-    return(
-        <div className={"bg-neutral-900 flex flex-col justify-center mx-60 pb-10 "}>
-            <div className={"font-integral-medium text-3xl font-light text-white text-center pt-10"}>
-                Nasze portfolio
+    const pathname = usePathname();
 
+    console.log("PortfolioPage rendered with pathname:", pathname);
+
+    return (
+        <motion.div
+            key={pathname}
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={transition1}
+            className="bg-neutral-900 flex flex-col justify-center mx-60 pb-10"
+        >
+            <div className="font-integral-medium text-3xl font-light text-white text-center pt-10">
+                Nasze portfolio
             </div>
 
-            <div className={"font-integral-medium text-xl font-light text-white text-center pt-5"}>
+            <div className="font-integral-medium text-xl font-light text-white text-center pt-5">
                 Kilka naszych przykładowych prac prezentujących co oferujemy
             </div>
 
-            <div className={"flex flex-row flex-wrap gap-3 justify-center pt-10"}>
-
-                <div className={"flex flex-col bg-neutral-100"}>
-                    <div className="overflow-hidden relative group">
-                        <div
-                            className="flex flex-col items-center justify-center transition-transform duration-300 transform ease-in-out hover:scale-110 hover:brightness-75">
-
-                            <Image
-                                src={Cover1}
-                                alt="Cover 1"
-                                width="400"
-                                className="cursor-pointer"
-                            />
-
+            <motion.div
+                initial={{ opacity: 0, y: "50%" }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: "50%" }}
+                transition={transition1}
+                className="flex flex-row flex-wrap gap-3 justify-center pt-10"
+            >
+                {[Cover1, Cover2, Cover3, Cover4, Cover5].map((cover, index) => (
+                    <div key={index} className="flex flex-col bg-neutral-100">
+                        <div className="overflow-hidden relative group">
+                            <div className="flex flex-col items-center justify-center transition-transform duration-300 transform ease-in-out hover:scale-110 hover:brightness-75">
+                                <Image
+                                    src={cover}
+                                    alt={`Cover ${index + 1}`}
+                                    width="400"
+                                    className="cursor-pointer"
+                                />
+                            </div>
+                        </div>
+                        <div className="text-black font-integral-medium font-light">
+                            {[
+                                "Studniówka Pionki Luty 2025",
+                                "Wesele Eweliny & Patryka Sierpień 2024",
+                                "18 urodziny Igi 2024",
+                                "HUNTRUN 2024 Białka Tatrzańska",
+                                "Teledysk BINKIEWICZ - Ludzie Mówią",
+                            ][index]}
                         </div>
                     </div>
-
-
-                    <div className={"text-black font-integral-medium font-light "}>
-                        Studniówka Pionki Luty 2025
-                    </div>
-                </div>
-
-                <div className={"flex flex-col bg-neutral-100"}>
-                    <div className="overflow-hidden relative group">
-                        <div
-                            className="flex flex-col items-center justify-center transition-transform duration-300 transform ease-in-out hover:scale-110 hover:brightness-75">
-
-                            <Image
-                                src={Cover2}
-                                alt="Cover 2"
-                                width="400"
-                                className="cursor-pointer"
-                            />
-
-                        </div>
-                    </div>
-
-                    <div className={"text-black font-integral-medium font-light "}>
-                        Wesele Eweliny & Patryka Sierpień 2024
-                    </div>
-                </div>
-
-                <div className={"flex flex-col bg-neutral-100"}>
-                    <div className="overflow-hidden relative group">
-                        <div
-                            className="flex flex-col items-center justify-center transition-transform duration-300 transform ease-in-out hover:scale-110 hover:brightness-75">
-
-                            <Image
-                                src={Cover3}
-                                alt="Cover 3"
-                                width="400"
-                                className="cursor-pointer"
-                            />
-
-                        </div>
-                    </div>
-
-                    <div className={"text-black font-integral-medium font-light "}>
-                        18 urodziny Igi 2024
-                    </div>
-                </div>
-
-                <div className={"flex flex-col bg-neutral-100"}>
-                    <div className="overflow-hidden relative group">
-                        <div
-                            className="flex flex-col items-center justify-center transition-transform duration-300 transform ease-in-out hover:scale-110 hover:brightness-75">
-
-                            <Image
-                                src={Cover4}
-                                alt="Cover 4"
-                                width="400"
-                                className="cursor-pointer"
-                            />
-
-                        </div>
-                    </div>
-
-                    <div className={"text-black font-integral-medium font-light "}>
-                        HUNTRUN 2024 Białka Tatrzańska
-                    </div>
-                </div>
-
-                <div className={"flex flex-col bg-neutral-100"}>
-                    <div className="overflow-hidden relative group">
-                        <div
-                            className="flex flex-col items-center justify-center transition-transform duration-300 transform ease-in-out hover:scale-110 hover:brightness-75">
-
-                            <Image
-                                src={Cover5}
-                                alt="Cover 5"
-                                width="400"
-                                className="cursor-pointer"
-                            />
-
-                        </div>
-                    </div>
-
-                    <div className={"text-black font-integral-medium font-light "}>
-                        Teledysk BINKIEWICZ - Ludzie Mówią
-                    </div>
-                </div>
-
-
-            </div>
-
-        </div>
-    )
+                ))}
+            </motion.div>
+        </motion.div>
+    );
 }
