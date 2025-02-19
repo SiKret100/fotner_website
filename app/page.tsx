e7b74e172d75c3a682fc5a9eb873aabd16c4bcd1
Dawid Rutkowski <dawidrut01@gmail.com>
2025-02-19 21:46:25 +0100

    add buttons on home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import img_bartek from "@/public/homepage/img_bartek.png"
 import mini_camera from "@/public/homepage/mini_camera.png"
 import React from 'react'
 import Image from "next/image";
+import Link from "next/link";
 
 
 const transition1 = {
@@ -15,7 +16,7 @@ const transition1 = {
 export default function Home() {
     return (
         <div className={"bg-neutral-900 min-h-screen"}>
-            <div className={"flex flex-row justify-between border-white border-2 "}>
+            <div className={"pt-8 flex flex-row justify-between border-white border-2 "}>
                 <Image src={img_dawid}
                        alt={"Image dawid"}
                        height={600}
@@ -24,8 +25,9 @@ export default function Home() {
 
                 <div className={"flex flex-col my-10"}>
 
-                    <div className={"bg-white border-2 text-center text-4xl font-integral-medium flex flex-row justify-center items-center gap-2"}>
-                       Fotner
+                    <div
+                        className={"bg-white border-2 text-center text-4xl font-integral-medium flex flex-row justify-center items-center gap-2"}>
+                        Fotner
 
                         <Image
                             src={mini_camera}
@@ -36,7 +38,8 @@ export default function Home() {
                         Studio
                     </div>
 
-                    <div className={"text-white text-3xl font-integral-medium border-2 py-3 my-4 flex flex-col gap-3 justify-center text-center"}>
+                    <div
+                        className={"text-white text-3xl font-integral-medium border-2 py-3 my-4 flex flex-col gap-3 justify-center text-center px-2"}>
                         <div>
                             Studniówki
                         </div>
@@ -62,7 +65,7 @@ export default function Home() {
                         </div>
 
                         <div>
-                            teledyski
+                            teledysi
                         </div>
 
                         <div className={"text-black stroke-2 stroke-white outline-2 border-solid"} style={{
@@ -76,8 +79,34 @@ export default function Home() {
                         <div>
                             wesela
                         </div>
+                    </div>
+
+                    <div
+                        className={"text-white font-integral-medium text-2xl font-bold  flex flex-row  justify-between"}>
+                        <Link href={'/'}>
+                            <div
+                                className={'bg-black  p-2 hover:bg-white hover:text-black transition ease-out duration-200 border-2 border-whited'}>
+                                Kontakt
+                            </div>
+                        </Link>
+
+                        <Link href={'/'}>
+                            <div
+                                className={'bg-black p-2 hover:bg-white hover:text-black transition ease-out duration-200 border-2 border-whited'}>
+                                Nasza praca
+                            </div>
+                        </Link>
+
+                        <Link href={'/'}>
+                            <div
+                                className={'bg-black  p-2 hover:bg-white hover:text-black transition ease-out duration-200 border-2 border-whited'}>
+                                Oferta
+                            </div>
+                        </Link>
 
                     </div>
+
+
 
 
                 </div>
